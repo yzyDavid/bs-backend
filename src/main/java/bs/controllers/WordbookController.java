c3b6ed4af7fe9ed2b8214d8116dc05db1ceeb840
Zhenyun Yu <yzyDavid@qq.com>
2018-06-22 19:35:49 +0800

    fix: change to POST of /workbook/words.

--- src/main/java/bs/controllers/WordbookController.java
+++ src/main/java/bs/controllers/WordbookController.java
@@ -16,10 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -74,7 +71,7 @@ public class WordbookController {
      * @return
      */
     @Authorization
-    @GetMapping(path = "/words")
+    @PostMapping(path = "/words")
     public ResponseEntity<WordsResponse> wordsOfWordbook(@RequestBody AddWordbookRequest request, @CurrentUser UserEntity user) {
         String wordbookName = request.getWordbook();
         if ((wordbookName == null) || "".equals(wordbookName)) {
